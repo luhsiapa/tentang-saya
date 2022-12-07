52aae59dc9cc2b63471cc1d0df5e75c1913da12a
Ferry Ayunda <119156813+luhsiapa@users.noreply.github.com>
2022-12-07 12:16:26 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,13 @@
 
 
 <div class="container">
-  <h1><a href="http://www.birkof.ro"><em>F</em>erry <em>A</em>yunda</a></h1>
+  <h1><a href="/"><em>F</em>erry <em>A</em>yunda</a></h1>
   <p class="slogan"><span>I code {name}.</span></p>
   <div class="contact">
-    <a href="https://twitter.com/birkof" target="_blank"><i class="icon-twitter"></i></a>
-    <a href="https://facebook.com/birkof" target="_blank"><i class="icon-facebook"></i></a>
-    <a href="https://linkedin.com/in/birkof" target="_blank"><i class="icon-linkedin"></i></a>
-    <a href="https://github.com/birkof" target="_blank"><i class="icon-github"></i></a>
+    <a href="https://twitter.com/" target="_blank"><i class="icon-twitter"></i></a>
+    <a href="https://facebook.com/" target="_blank"><i class="icon-facebook"></i></a>
+    <a href="https://linkedin.com/in/" target="_blank"><i class="icon-linkedin"></i></a>
+    <a href="https://github.com/luhsiapa" target="_blank"><i class="icon-github"></i></a>
   </div>
 </div>
 
