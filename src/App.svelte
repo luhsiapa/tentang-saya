255f911d6d2b59981a50831980baf2ecf9fbd959
Ferry Ayunda <119156813+luhsiapa@users.noreply.github.com>
2022-12-07 12:36:47 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
   <p class="slogan"><span>I code {name}.</span></p>
   <div class="contact">
     <a href="https://twitter.com/" target="_blank"><i class="icon-twitter"></i></a>
-    <a href="https://facebook.com/" target="_blank"><i class="icon-facebook"></i></a>
+    <a href="https://www.facebook.com/bungferry.wa" target="_blank"><i class="icon-facebook"></i></a>
     <a href="https://linkedin.com/in/" target="_blank"><i class="icon-linkedin"></i></a>
     <a href="https://github.com/luhsiapa" target="_blank"><i class="icon-github"></i></a>
   </div>
