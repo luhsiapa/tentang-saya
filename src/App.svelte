eb97abaaa193b909e71f8c604211a8749d997c36
Ferry Ayunda <119156813+luhsiapa@users.noreply.github.com>
2022-12-07 12:01:29 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let name;
+</script>
+
+
+<div class="container">
+  <h1><a href="http://www.birkof.ro"><em>F</em>erry <em>A</em>yunda</a></h1>
+  <p class="slogan"><span>I code {name}.</span></p>
+  <div class="contact">
+    <a href="https://twitter.com/birkof" target="_blank"><i class="icon-twitter"></i></a>
+    <a href="https://facebook.com/birkof" target="_blank"><i class="icon-facebook"></i></a>
+    <a href="https://linkedin.com/in/birkof" target="_blank"><i class="icon-linkedin"></i></a>
+    <a href="https://github.com/birkof" target="_blank"><i class="icon-github"></i></a>
+  </div>
+</div>
+
